cd25ca3c7533d8c08c25a734de6aefc0745da998
Rudys Natanael Acosta <natanael926@gmail.com>
2023-08-12 02:55:00 -0400

    Firt step

---
+++ src/TiendaElectronica.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/**
+ * Esta clase representa un ejemplo basico de Inner Classes
+ *
+ * Supongamos que estamos creando un programa para modelar una tienda
+ * de productos electrónicos con empleados y productos:
+ */
+public class TiendaElectronica {
+    private String nombre;
+    private List<Empleado> empleados;
+
+    public TiendaElectronica(String nombre) {
+
+    }
+
+
+    private class Empleado {
+
+    }
+}
